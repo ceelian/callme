9f270485db657e132c5091d0bcbcfdcc4dea9bee
skudriashev <skudriashev@griddynamics.com>
2014-02-21 00:19:13 +0200

    Added forgotten header

--- src/callme/tests/unit/test_protocol.py
+++ src/callme/tests/unit/test_protocol.py
@@ -1,10 +1,42 @@
-from callme import protocol
+# Copyright (c) 2009-2014, Christian Haintz
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#
+#     * Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
+#       with the distribution.
+#
+#     * Neither the name of callme nor the names of its contributors
+#       may be used to endorse or promote products derived from this
+#       software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import unittest
+
+from callme import protocol
 
 
 class TestRpcRequest(unittest.TestCase):
 
-    def test_constructor(self):
+    def test_creation(self):
         func_args = {'arg1': 3, 'arg2': 7}
         request = protocol.RpcRequest("func_name", func_args)
         self.assertEqual(request.func_name, "func_name")
@@ -13,12 +45,12 @@ class TestRpcRequest(unittest.TestCase):
 
 class TestRpcResponse(unittest.TestCase):
 
-    def test_constructor_default(self):
+    def test_creation_default(self):
         response = protocol.RpcResponse("result")
         self.assertEqual(response.result, "result")
         self.assertFalse(response.is_exception)
 
-    def test_constructor_exception(self):
+    def test_creation_exception(self):
         response = protocol.RpcResponse(Exception("test"))
         self.assertIsInstance(response.result, Exception)
         self.assertTrue(response.is_exception)
